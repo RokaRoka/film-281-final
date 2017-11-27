8bb0e9d1be53a79961d640f1d6a3e7c579085131
Samuel Scott <samuel.scott@mymail.champlain.edu>
2017-11-27 16:02:29 -0500

    Added more positions to screen_data.lua

--- resources/constants/screen_data.lua
+++ resources/constants/screen_data.lua
@@ -10,6 +10,13 @@ s_d.positions = {}
 s_d.positions.top_left = vector.new(0, 0)
 s_d.positions.top_center = vector.new(width/2, 0)
 s_d.positions.top_right = vector.new(width, 0)
+
+s_d.positions.left = vector.new(0, height/2)
 s_d.positions.center = vector.new(width/2, height/2)
+s_d.positions.right = vector.new(width, height/2)
+
+s_d.positions.bot_left = vector.new(0, height)
+s_d.positions.bot_center = vector.new(width/2, height)
+s_d.positions.bot_right = vector.new(width, height)
 
 return s_d
