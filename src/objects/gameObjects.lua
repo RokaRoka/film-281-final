626c75247ca93fdffbf8289d7650db31215bcc54
samscott <samuel.scott@mymail.champlain.edu>
2017-11-28 03:03:28 -0500

    Fix gameObjects to return a table with Player Class.

--- src/objects/gameObjects.lua
+++ src/objects/gameObjects.lua
@@ -12,30 +12,32 @@ local physics = require("src.re-usable.physicsClasses")
 local graphics = require("src.re-usable.graphicsClasses")
 local window = require("src.re-usable.windowClasses")
 
-Player = Class{__includes = base.Object,
+local gObj = {}
+
+gObj.Player = Class{__includes = Object,
 	init = function(self, x, y)
 		base.Object.init(self, x, y, 32, 32)
 
 		self.in_range_of = {}
 
 		self.drawable = true
-	end,
 
-  currentSpeed = vector(0, 0),
+		self.currentSpeed = vector.new(0, 0)
+	end,
 
 	--player default values
   acceleration = 10,
   maxVelocity = 15
 }
 
-function Player:update(dt)
-	if not player.busy then
+function gObj.Player:update(dt)
+	if not self.busy then
 		self:walk(dt)
 		self:checkAction(dt)
 	end
 end
 
-function Player:walk(dt)
+function gObj.Player:walk(dt)
 	local dx, dy = 0, 0
 	if love.keyboard.isDown('left') then
         dx = -1 * maxVelocity
@@ -50,26 +52,26 @@ function Player:walk(dt)
     self:move(dt, dx, dy)
 end
 
-function Player:move(dt, dx, dy)
+function gObj.Player:move(dt, dx, dy)
 	local delta = vector(dx, dy)
 	--work on this a bit more
-  currentSpeed.x = currentSpeed.x + (delta.x - currentSpeed.x)
-	self.p_body.body:setLinearVelocity(, delta.y * (dt * 100))
-	local newPx, newPy = self.p_body.body:getPosition()
+  --self.currentSpeed.x = self.currentSpeed.x + (delta.x - self.currentSpeed.x)
+	--self.p_body.body:setLinearVelocity(, delta.y * (dt * 100))
+	--local newPx, newPy = self.p_body.body:getPosition()
 	newPx = math.floor(newPx)
 	newPy = math.floor(newPy)
 
 	self.pos = vector.new(newPx, newPy)
 end
 
-function Player:checkAction(dt)
-	if self.in_range_of[1] and key_press.check("z") then
-		self.debug.text = "Player talking!"
-		self.in_range_of[1].interact:response()
+function gObj.Player:checkAction(dt)
+	if self.in_range_of[1] then
+		--self.debug.text = "Player talking!"
+		--self.in_range_of[1].interact:response()
 	end
 end
 
-function Player:draw()
+function gObj.Player:draw()
 	if self.debug_img and self.debug.drawable then
 		self.debug_img:draw()
 	elseif self.currentAnim then
@@ -77,3 +79,5 @@ function Player:draw()
 	end
 	love.graphics.print(self.debug.text, 600, 10)
 end
+
+return gObj
