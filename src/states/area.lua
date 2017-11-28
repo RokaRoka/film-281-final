5c97388f4eb0c0e3bec4dfffe5f9a18fd2d3c210
Samuel Scott <samuel.scott@mymail.champlain.edu>
2017-11-28 01:47:22 -0500

    Initialize area state.

--- src/states/area.lua
+++ src/states/area.lua
@@ -1,8 +1,48 @@
 --required library
 --hump
 local Gamestate = require("libraries.hump.gamestate")
+local vector = require("libraries.hump.vector")
+
+--required src
+--functions
+local textFunctions = require("src.functions.textFunctions")
+--objects
+local gameObjects = require("src.objects.gameObjects")
+
+--required resources
+--constants
+local screen_data = require("resources.constants.screen_data")
 
 --area callbacks
 local area = {}
+
+function area:init()
+
+end
+
+function area:enter(previous, args)
+  local Gamestates = args.GameStates
+
+end
+
+function area:update(dt)
+  --update stuff
+end
+
+function area:draw()
+  --draw stuff
+  end
+
+function area:keyreleased()
+
+end
+
+function area:keypressed(key, scancode, isrepeat)
+
+end
+
+function area:mousereleased()
+
+end
 
 return area
